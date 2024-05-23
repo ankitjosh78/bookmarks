3608c394cc7e3ea796c0925614c18b0cdbc8ee5e
Ankit Josh <ankitjosh78@protonmail.com>
2024-05-24 02:18:32 +0530

    Added Upload image feature and username edit

--- images/views.py
+++ images/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, \
-                                  PageNotAnInteger
-from .forms import ImageCreateForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import ImageCreateForm, ImageUploadForm
 from .models import Image
 from actions.utils import create_action
 import redis
@@ -15,14 +14,14 @@ from django.conf import settings
 
 
 # connect to redis
-r = redis.Redis(host=settings.REDIS_HOST,
-                port=settings.REDIS_PORT,
-                db=settings.REDIS_DB)
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
 def image_create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # form is sent
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
@@ -32,57 +31,78 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
-            create_action(request.user, 'bookmarked image', new_image)
-            messages.success(request, 'Image added successfully')
+            create_action(request.user, "bookmarked image", new_image)
+            messages.success(request, "Image added successfully")
             # redirect to new created image detail view
             return redirect(new_image.get_absolute_url())
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-    return render(request,
-                  'images/image/create.html',
-                  {'section': 'images',
-                   'form': form})
+    return render(
+        request, "images/image/create.html", {"section": "images", "form": form}
+    )
+
+@login_required
+def image_upload(request):
+    if request.method == "POST":
+        # form is sent
+        form = ImageUploadForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # assign current user to the item
+            new_image.user = request.user
+            new_image.save()
+            create_action(request.user, "bookmarked image", new_image)
+            messages.success(request, "Image added successfully")
+            # redirect to new created image detail view
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageUploadForm()
+    return render(
+        request, "images/image/upload.html", {"section": "images", "form": form}
+    )
 
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
-    total_views = r.incr(f'image:{image.id}:views')
+    total_views = r.incr(f"image:{image.id}:views")  # type: ignore
     # increment image ranking by 1
-    r.zincrby('image_ranking', 1, image.id)
-    return render(request,
-                  'images/image/detail.html',
-                  {'section': 'images',
-                   'image': image,
-                   'total_views': total_views})
+    r.zincrby("image_ranking", 1, image.id)  # type: ignore
+    return render(
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
+    )
 
 
 @login_required
 @require_POST
 def image_like(request):
-    image_id = request.POST.get('id')
-    action = request.POST.get('action')
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
-            if action == 'like':
+            if action == "like":
                 image.users_like.add(request.user)
-                create_action(request.user, 'likes', image)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status': 'ok'})
+            return JsonResponse({"status": "ok"})
         except Image.DoesNotExist:
             pass
-    return JsonResponse({'status': 'error'})
+    return JsonResponse({"status": "error"})
 
 
 @login_required
 def image_list(request):
     images = Image.objects.all()
     paginator = Paginator(images, 8)
-    page = request.GET.get('page')
-    images_only = request.GET.get('images_only')
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
     try:
         images = paginator.page(page)
     except PageNotAnInteger:
@@ -92,31 +112,30 @@ def image_list(request):
         if images_only:
             # If AJAX request and page out of range
             # return an empty page
-            return HttpResponse('')
+            return HttpResponse("")
         # If page out of range return last page of results
         images = paginator.page(paginator.num_pages)
     if images_only:
-        return render(request,
-                      'images/image/list_images.html',
-                      {'section': 'images',
-                       'images': images})
-    return render(request,
-                  'images/image/list.html',
-                   {'section': 'images',
-                    'images': images})
+        return render(
+            request,
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
+        )
+    return render(
+        request, "images/image/list.html", {"section": "images", "images": images}
+    )
 
 
 @login_required
 def image_ranking(request):
     # get image ranking dictionary
-    image_ranking = r.zrange('image_ranking', 0, -1,
-                             desc=True)[:10]
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]  # type: ignore
     image_ranking_ids = [int(id) for id in image_ranking]
     # get most viewed images
-    most_viewed = list(Image.objects.filter(
-                           id__in=image_ranking_ids))
-    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
-    return render(request,
-                  'images/image/ranking.html',
-                  {'section': 'images',
-                   'most_viewed': most_viewed})
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))  # type: ignore
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed},
+    )
